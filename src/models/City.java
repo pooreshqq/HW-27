19e8da6b622a64c427c8a2c52130c774852e3630
Kutman <ipomea875@gmail.com>
2026-04-23 02:12:42 +0600

    I modified the City class by adding the name and demandedGoods fields, and also wrote a constructor and getters for the fields.

--- src/models/City.java
+++ src/models/City.java
@@ -1,4 +1,21 @@
 package models;
 
+import java.util.List;
+
 public class City {
+     private String name;
+     private List<String> demandedGoods;
+
+     public City(String name, List<String> demandedGoods){
+         this.name = name;
+         this.demandedGoods = demandedGoods;
+     }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getDemandedGoods() {
+        return demandedGoods;
+    }
 }
