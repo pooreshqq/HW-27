185c160a28e239112ff487bc6bf4ae523350bf1c
Aktan <aktandzmbkv@gmail.com>
2026-04-23 00:07:22 +0600

    Added private fields: weight, type, quality, price
    Added getters and constructor

--- src/models/Goods.java
+++ src/models/Goods.java
@@ -1,4 +1,42 @@
 package models;
 
+import enums.GoodsQuality;
+import enums.GoodsType;
+
 public class Goods {
+    private int weight;
+    private GoodsType type;
+    private GoodsQuality quality;
+    private int price;
+
+    public Goods(int weight, GoodsType type, GoodsQuality quality, int price) {
+        this.weight = weight;
+        this.type = type;
+        this.quality = quality;
+        this.price = price;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public GoodsType getType() {
+        return type;
+    }
+
+    public GoodsQuality getQuality() {
+        return quality;
+    }
+
+    public double getQualityCoefficient() {
+        return quality.getCOEFFICIENT();
+    }
+
+    public String getQualityDescription() {
+        return quality.getDESCRIPTION();
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
