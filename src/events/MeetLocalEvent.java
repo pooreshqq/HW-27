f07ca06ef3904c0e0b470bd713e7658020193c70
Kutman <ipomea875@gmail.com>
2026-04-24 03:21:52 +0600

    I added a MeetLocalEvent class that implements the methods of the Event interface.

---
+++ src/events/MeetLocalEvent.java
@@ -0,0 +1,19 @@
+package events;
+
+import models.Trader;
+
+import java.util.Random;
+
+public class MeetLocalEvent implements Event{
+
+    @Override
+    public String getName() {
+        return "Встретил местного";
+    }
+
+    @Override
+    public void apply(Trader trader, Random random) {
+        int bonus = 3 + random.nextInt(3) + 1;
+        trader.travel(trader.getSpeed() + bonus);
+    }
+}
