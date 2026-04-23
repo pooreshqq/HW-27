2f34d377ed32685c37864ef5460c91fa77618119
Kutman <ipomea875@gmail.com>
2026-04-24 02:30:24 +0600

    I modified the Trader class by converting the move method to travel . I added the OrdinaryDayEvent class, which implemented the Event interface methods.

---
+++ src/events/OrdinaryDayEvent.java
@@ -0,0 +1,18 @@
+package events;
+
+import models.Trader;
+
+import java.util.Random;
+
+public class OrdinaryDayEvent implements Event{
+
+    @Override
+    public String getName() {
+        return "Обычный день";
+    }
+
+    @Override
+    public void apply(Trader trader, Random random) {
+        trader.travel(trader.getSpeed());
+    }
+}
