ccf32f48a3f4d4691b851124da8c80544de7a60d
Aktan <aktandzmbkv@gmail.com>
2026-04-23 16:06:43 +0600

    Added StateSelectionException for throwing exceptions in state selection scenarios (in sum it's for "State" pattern)

---
+++ src/exceptions/StateSelectionException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class StateSelectionException extends Exception {
+    public StateSelectionException() {
+        super();
+    }
+    public StateSelectionException(String message) {
+        super(message);
+    }
+}
