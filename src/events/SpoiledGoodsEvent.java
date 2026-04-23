a3081258b92b0814bc610ab0abe65cd6ed26d93c
Kutman <ipomea875@gmail.com>
2026-04-24 04:04:08 +0600

    I added the SpoiledGoodsEvent class, which implements the Event interface methods.

---
+++ src/events/SpoiledGoodsEvent.java
@@ -0,0 +1,25 @@
+package events;
+
+import models.Goods;
+import models.Trader;
+
+import java.util.List;
+import java.util.Random;
+
+public class SpoiledGoodsEvent implements Event{
+    @Override
+    public String getName() {
+        return "Товар испортился";
+    }
+
+    @Override
+    public void apply(Trader trader, Random random) {
+        List<Goods> goods = trader.getGoodsList();
+        if (!goods.isEmpty()) {
+            Goods spoiled = goods.get(random.nextInt(goods.size()));
+            spoiled.downgradeState();
+        }
+
+        trader.travel(trader.getSpeed());
+    }
+}
