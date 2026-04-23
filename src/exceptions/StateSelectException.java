62b7b4ad4457250ff54d898c8a4e749d19c929e0
Aktan <aktandzmbkv@gmail.com>
2026-04-23 16:48:26 +0600

    Renamed StateSelectionException to StateSelectException

---
+++ src/exceptions/StateSelectException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class StateSelectException extends Exception {
+    public StateSelectException() {
+        super();
+    }
+    public StateSelectException(String message) {
+        super(message);
+    }
+}
