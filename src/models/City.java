4006a66613f1e3bb3865cd2733cc7394169f861a
Aktan <aktandzmbkv@gmail.com>
2026-04-24 17:08:09 +0600

    Removed demandedGoods field and methods related to it

--- src/models/City.java
+++ src/models/City.java
@@ -4,19 +4,13 @@ import java.util.List;
 
 public class City {
      private String name;
-     private List<String> demandedGoods;
 
-     public City(String name, List<String> demandedGoods){
+     public City(String name){
          this.name = name;
-         this.demandedGoods = demandedGoods;
      }
 
     public String getName() {
         return name;
-    }
-
-    public List<String> getDemandedGoods() {
-        return demandedGoods;
     }
 
     @Override
